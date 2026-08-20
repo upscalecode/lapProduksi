d86b5499878c952648bd02129fd4b6b0fa13bd9f
upscalecode <arikubuntu@gmail.com>
2026-08-20 10:30:50 +0700

    Deploy ulang WepApp Script

--- script.js
+++ script.js
@@ -38,7 +38,7 @@
     PRESS_BALANCE_PAGE_SIZE: 10,
 
     // Ganti dengan URL deployment Web App terbaru yang berakhir /exec.
-    WEB_APP_URL: "https://script.google.com/macros/s/AKfycbzq8ub_Q5ycPXScB64p2Wbm-Rmk5ydPdf6QEpQsDmasMNbCdkB0O439evrQr2pwkoC8UA/exec"
+    WEB_APP_URL: "https://script.google.com/macros/s/AKfycbzVilOt10kroTiGqNsVDye1i1VP3fDHeDBIkHoiedZ_VLuyQC_gjiUzGI_KGqXrgEwHeg/exec"
   };
 
   const LINE_LABEL = { filling: "Filling", press: "Press" };
